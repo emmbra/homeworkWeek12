1159bf311f39380ff26fd49e52f05878cae0c149
Emmett Brady <emmett.brady@gmail.com>
2020-04-26 14:44:16 -0700

    removed notes throughout file

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-// const inquirer = require("inquirer");
-// const mysql = require("mysql");
-
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "employee_trackerDB",
-// });
-
-// connection.connect(function (err) {
-//   if (err) throw err;
-//   console.log("connected as id " + connection.threadId);
-//   mainMenu();
-// });
-
 const mysql = require("mysql");
 const util = require("util");
 const inquirer = require("inquirer");
@@ -30,6 +13,7 @@ if (process.env.JAWSDB_URL) {
     database: "employee_trackerDB",
   };
 }
+
 function makeDb(config) {
   const connection = mysql.createConnection(config);
   return {
